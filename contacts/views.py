1b8f51c81d4140fd72055ecb155fe0f803dff4df
Nathan Murrow <nathanmurrow@gmail.com>
2018-07-14 00:48:48 -0700

    add http basic authentication to api

--- contacts/views.py
+++ contacts/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.http import (
-    JsonResponse, HttpResponseBadRequest, HttpResponseNotFound)
+    JsonResponse,
+    HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden)
+from django.contrib.auth import authenticate
 from contacts.models import Contact
 import json
+from base64 import b64decode
 
+
+def require_authentication(fn):
+    def authenticator(request, *args):
+        try:
+            method, encoded = request.META['HTTP_AUTHORIZATION'].split()
+            assert method.lower() == 'basic'
+            username, password = b64decode(encoded).decode().split(':')
+            request.user = authenticate(username=username, password=password)
+            assert request.user.is_authenticated
+        except:
+            return HttpResponseForbidden()
+        return fn(request, *args)
+
+    return authenticator
+
+
+@require_authentication
 def create_contact(request):
     c = Contact.objects.create(**json.loads(request.body))
     return JsonResponse(c.json)
+
 
 def query_contacts(request):
     if request.body:
@@ -15,10 +36,13 @@ def query_contacts(request):
         cs = Contact.objects.all()
     return JsonResponse([c.json for c in cs], safe=False)
 
+
 def get_contact(request, pk):
     c = Contact.objects.get(pk=pk)
     return JsonResponse(c.json)
 
+
+@require_authentication
 def update_contact(request, pk):
     cs = Contact.objects.filter(pk=pk)
     if cs.count() != 1:
@@ -26,11 +50,14 @@ def update_contact(request, pk):
     cs.update(**json.loads(request.body))
     return JsonResponse(cs[0].json)
 
+
+@require_authentication
 def delete_contact(request, pk):
     c = Contact.objects.get(pk=pk)
     deleted_json = c.json
     c.delete()
     return JsonResponse(deleted_json)
+
 
 def call_api_pk(request, pk):
     try:
@@ -45,6 +72,7 @@ def call_api_pk(request, pk):
     except:
         pass
     return HttpResponseBadRequest()
+
 
 def call_api(request):
     try:
